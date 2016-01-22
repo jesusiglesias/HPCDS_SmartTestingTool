bf8cbc58d74705dc3472f4f7cfa42e16f794364a
Jesús Iglesias <jesusgiglesias@gmail.com>
2016-01-22 20:14:21 +0100

    _Events updated and template setup
    Display-name element has been changed in web.xml template and in _Events file has been added several tasks.

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -2,12 +2,18 @@
  * Script to perform actions at different times
  */
 
+// Read properties from file
+ant.property file: 'application.properties'
+def appName = ant.project.properties.'app.name'
+
 // LogConfig.groovy path
 def LogConfigPath = "${basedir}/grails-app/conf/LogConfig.groovy"
 // DBConfig.groovy path
 def DBConfigPath = "${basedir}/grails-app/conf/DBConfig.groovy"
 // logs dir path
 def deleteLogDir = "${basedir}/logs"
+// .log file path
+def deleteLogFile = "/tmp/${appName}_Error.log"
 
 // It adds file to destination path during war creation event
 eventCreateWarStart = { warName, stagingDir ->
@@ -18,14 +24,31 @@ eventCreateWarStart = { warName, stagingDir ->
     }
 }
 
-// It deletes the "logs" directory and its contents recursively
+// It performs tasks when war creation event finishes
 eventCreateWarEnd = {warName, stagingDir ->
 
     def devEnv = 'development'
     def testEnv = 'test'
+    def prodEnv = "production"
 
+    // It deletes the "logs" directory and its contents recursively
     if (System.getProperty('grails.env') == devEnv ||  System.getProperty('grails.env') == testEnv) {
         print ("_Events():CreateWarEnd:deleteDir:logs")
         ant.delete(dir:deleteLogDir)
     }
+
+    // It deletes log in /tmp directory
+    if (System.getProperty('grails.env') == prodEnv) {
+        print ("_Events():CreateWarEnd:deleteFile:.log")
+        ant.delete(file:deleteLogFile)
+    }
+}
+
+// It modifies the "display-name" element in web.xml during webxml creation event
+eventWebXmlStart = { webXmlFile ->
+
+    print("_Events():WebXmlStart:display_name")
+    def tmpWebXmlFile = new File(projectWorkDir, webXmlFile)
+    ant.replace(file: tmpWebXmlFile, token: "@grails.app.version@",
+            value: "${grailsAppVersion}")
 }
