27d37d88de235bad30ec4d3d675d32c47bd61d32
Jesús Iglesias <jesusgiglesias@gmail.com>
2016-01-23 14:06:25 +0100

    Comments updated in custom authentication service

--- grails-app/services/Security/CustomSecUserDetailsService.groovy
+++ grails-app/services/Security/CustomSecUserDetailsService.groovy
@@ -18,14 +18,37 @@ class CustomSecUserDetailsService implements GrailsUserDetailsService {
     // no granted roles this one which gets * past that restriction but doesn't grant anything.
     static final List NO_ROLES = [new SimpleGrantedAuthority(SpringSecurityUtils.NO_ROLE)]
 
+    /**
+     * Locates the user based on the username.
+     *
+     * @param username The username identifying the user whose data is required.
+     * @param loadRoles Whether to load roles at the same time as loading the user.
+     *
+     * @return A fully populated user record (never <code>null</code>)
+     *
+     * @throws UsernameNotFoundException If the user could not be found
+     */
     UserDetails loadUserByUsername(String username, boolean loadRoles) throws UsernameNotFoundException {
         return loadUserByUsername(username)
     }
 
+    /**
+     * Locates the user based on the email or username.
+     *
+     * @param username The username identifying the user whose data is required.
+     *
+     * @return A fully populated user record (never <code>null</code>)
+     *
+     * @throws UsernameNotFoundException If the user could not be found
+     */
     UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
         SecUser.withTransaction { status ->
+
+            // It searches the user by email or username introduced
             SecUser user = SecUser.findByUsernameOrEmail(username, username)
+
+            // User doesn't exist
             if (!user) throw new UsernameNotFoundException('User not found', username)
 
             def authorities = user.authorities.collect {new SimpleGrantedAuthority(it.authority)}
