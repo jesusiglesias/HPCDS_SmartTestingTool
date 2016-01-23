cb9d73668dbcc412af2ce89ff848d57d6aee43f9
Jesús Iglesias <jesusgiglesias@gmail.com>
2016-01-23 21:55:08 +0100

    Environment division added

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -2,6 +2,7 @@
  *                                         BOOTSTRAP                                         *
  *-------------------------------------------------------------------------------------------*/
 
+import grails.util.Environment
 import Security.*
 
 /**
@@ -14,7 +15,33 @@ class BootStrap {
      */
     def init = { servletContext ->
 
-        // Populating the database
+        // Database populating depending on the environment
+        switch (Environment.current) {
+            case Environment.DEVELOPMENT:
+                createInitialUsersDevTest()
+                break;
+            case Environment.TEST:
+                createInitialUsersDevTest()
+                break;
+            case Environment.PRODUCTION:
+                createInitialUsersProd()
+                break;
+        }
+    }
+
+    /**
+     * Final operations when application finishes
+     */
+    def destroy = {
+    }
+
+    /**
+     * Populating the database in development or test environments
+     */
+    void createInitialUsersDevTest() {
+        log.debug("BootStrap:init():createInitialUsersDevTest")
+
+        // It checks data existence
         if (!SecUser.count() && !SecRole.count()) {
 
             // Creating roles
@@ -53,17 +80,57 @@ class BootStrap {
                 }
 
                 log.debug("BootStrap:init():Base users have been created")
+                log.info("Special config create for development or test: admin_stt/sttadmintfg and user_stt/sttusertfg")
             } else {
                 log.error("BootStrap:init():Base users have not been created")
             }
         } else {
-            log.debug("BooStrap:init():Existing user or role data")
+            log.warn("BooStrap:init():Existing user or role data")
         }
     }
 
     /**
-     * Final operations when application finishes
+     * Populating the database in production environment
      */
-    def destroy = {
+    void createInitialUsersProd() {
+        log.debug("BootStrap:init():createInitialUsersProd")
+
+        // It checks data existence
+        if (!SecUser.count() && !SecRole.count()) {
+
+            // Creating roles
+            def adminRole = SecRole.findByAuthority('ROLE_ADMIN') ?: new SecRole(authority: 'ROLE_ADMIN')
+            def userRole = SecRole.findByAuthority('ROLE_USER') ?: new SecRole(authority: 'ROLE_USER')
+
+            // Creating new users
+            def newAdmin = SecUser.findByUsername('admin_stt') ?: new SecUser(
+                    username: 'admin_stt',
+                    password: 'sttadminprod',
+                    email: 'admin_stt_prod@stt.com')
+
+            // Validation of new user
+            def validAdmin = newAdmin.validate()
+
+            if (validAdmin) {
+                // Saving roles
+                adminRole.save(flush: true, failOnError: true)
+                userRole.save(flush: true, failOnError: true)
+
+                // Saving new users
+                newAdmin.save(flush: true, failOnError: true)
+
+                // Assign user to role
+                if (!newAdmin.authorities.contains(adminRole)) {
+                    SecUserSecRole.create newAdmin, adminRole, true
+                }
+
+                log.debug("BootStrap:init():Admin users have been created")
+            } else {
+                log.error("BootStrap:init():Admin users have not been created")
+            }
+        } else {
+            log.warn("BooStrap:init():Existing admin or role data")
+        }
     }
+
 }
