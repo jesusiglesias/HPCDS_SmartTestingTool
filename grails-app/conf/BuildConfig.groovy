ae67d2eca66ae0bdb7c6a0c0fb23a5dbc94c5793
Jesús Iglesias <jesusgiglesias@gmail.com>
2016-02-21 00:30:16 +0100

    jQuery dependence deleted of BuildConfig

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -83,6 +83,5 @@ grails.project.dependency.resolution = {
         // Plugins needed at runtime but not for compilation
         runtime ":hibernate4:4.3.10" // or ":hibernate:3.6.10.18"
         runtime ":database-migration:1.4.0"
-        runtime ":jquery:1.11.1"
     }
 }
