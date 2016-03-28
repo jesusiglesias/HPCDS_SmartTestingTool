5a83a2fdc835d3ca11aaca93ea54c8bd65483c03
Jesús Iglesias <jesusgiglesias@gmail.com>
2016-03-28 18:31:58 +0200

    Avatar field deleted in user

--- grails-app/domain/User/User.groovy
+++ grails-app/domain/User/User.groovy
@@ -1,15 +1,16 @@
 package User
 
 import Enumerations.Sex
+import Security.SecUser
 
 /**
  * It represents the general user information.
  */
-class User {
+class User extends SecUser{
 
+    UUID id
     // Attributes
     String address
-    String avatar
     Date birthDate
     String city
     String country
@@ -19,7 +20,27 @@ class User {
     String surname
     Integer zipCode
 
+    // Relations between entities
+    //static belongsTo = [secUser: SecUser]
+    static belongsTo = [department:Department]
+    static hasMany = [evaluations:Evaluation]
+
+
     // Restrictions on the attributes of the entity
-    static constraints = {
+   /* static constraints = {
+        address blank: false, maxSize: 50
+        avatar  nullable: true, blank: false
+        city    blank: true
+        country nullable: true, blank: false
+        name    nullable: true, blank: true  name blank: false, unique: true, maxSize: 50
+        phone
+        sex
+        surname
+        zipCode         zipCode(blank: false, size: 5..5, validator: {val, obj -> val?.isNumber()})
+    }*/
+
+    // It modifies the id type
+    static mapping = {
+        id(generator: "uuid2", type: "uuid-binary", length: 16)
     }
 }
